d0a026f8ba7919509177108d40f00289e257c6d6
Bongane Zitha <bonga.zitha@gmail.com>
2020-02-18 14:11:54 +0200

    Add files via upload

---
+++ explo/__init__.py
@@ -0,0 +1,1 @@
+from . import explo_app
---
+++ explo/explo_app.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+
+
+
+def metric_dict(items):
+
+    """
+    Write a function that calculates metrics from Eskom Data and outputs the metrics as a dictionary. 
+    These metrics must include:
+        
+    > Mean
+    > Median
+    > Maximum
+    > Minimum
+    > Standard Deviation
+    > Variance
+
+    """
+
+
+def date_parser(dates):
+
+    """
+    This functions takes a date and converts it into a certain format.
+    """
+    
+    date = [datetime.split(' ')[0] for datetime in dates]
+    return date
+
+
+def extract_municipality_hastags(df):
+
+    """
+    This function must return a dataframe with:
+    An added column of extracted hashtags from each tweet.
+    An added column of the municipality mentioned in each tweet 
+    """
+
+
+
+def number_of_tweets_per_day(df):
+    
+    """
+    This function returns a number of tweets per day.
+    """
+    df['Date'] = [entry.split(' ')[0] for entry in df['Date']]
+    return df.groupby('Date').count()
+
+
+def stop_words_remover(df):
+
+    """
+    This function returns a dataframe without stop words
+    """
+
+
+def word_splitter(df):
+
+    """
+    This function takes a dataframe and returns a new dataframe with 
+    a new column of individual words
+    """
+    df['Split Tweets'] = [tweet.split(' ') for tweet in df['Tweets']]
+    return df
+
+
+def five_num_summary(items):
+
+    """
+    This function return a dictinary of the five number summary.
+    """
